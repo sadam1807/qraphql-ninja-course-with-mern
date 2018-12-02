cba6a8663c54f7a0102404504ea23aedfce9802b
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-02 16:08:07 +0530

    tested book type with graphiql

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const schema = require('./server/schema/schema');
 const  app = express();
 
 app.use('/graphql',graphqlHTTP ({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000,() => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
 const graphql = require('graphql');
 const _ = require('lodash')
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, 
+       GraphQLString, 
+       GraphQLSchema, 
+       GraphQLID} = graphql;
 
 //dummy data
 var books = [
@@ -10,12 +13,13 @@ var books = [
 ];
 
 
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genere: { type: GraphQLString }
+        genre: { type: GraphQLString }
     })
 })
 
@@ -24,9 +28,9 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: { type: GraphQLString }},
+            args: {id: { type: GraphQLID }},
             resolve( parent, args){
-               _.find(books, { id: args.id }) 
+              return _.find(books, { id: args.id }) 
             }
         }
     }
