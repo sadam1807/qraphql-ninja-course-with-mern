39942b22f1484b90acae06b064e673c4de9611ac
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-03 14:03:25 +0530

    Added validation using graqhql NonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,9 @@ const {GraphQLObjectType,
        GraphQLSchema, 
        GraphQLID, 
        GraphQLInt,
-       GraphQLList } = graphql;
+       GraphQLList,
+       GraphQLNonNull
+     } = graphql;
 
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
@@ -86,8 +88,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args:{
-                name: { type: GraphQLString},
-                age: { type: GraphQLInt}
+                name: { type: new GraphQLNonNull(GraphQLString)},
+                age: { type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args){
                 let author= new Author({
@@ -100,9 +102,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args:{
-                name: { type: GraphQLString},
-                genre: { type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: { type: new GraphQLNonNull(GraphQLString)},
+                genre: { type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book= new Book({
