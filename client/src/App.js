f62f84f4a749d45dbced524040db33d349e760d3
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-06 16:20:02 +0530

    fetched authors name

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 
 //components 
 import BookList from './components/BookList'
+import AddBook from './components/AddBook'
 
 //apollo client setup
 const client = new ApolloClient({
@@ -19,6 +20,7 @@ class App extends Component {
         <div id="main">
           <h1>Ninja's Reading List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider> 
     );
---
+++ client/src/components/AddBook.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {gql} from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getAuthorsQuery = gql`
+{
+  authors{
+    name
+    id
+  }
+}
+`
+
+class AddBook extends Component {
+    displayAuthors(){
+        var data = this.props.data;
+        if(data.loading){
+          return (
+             <option disabled>Loading Books....</option>
+          )
+        }
+        else {
+          return data.authors.map(author => {
+            return(
+              <option key={author.id} value={author.id}>{author.name}</option>
+           );
+          })
+        }
+      }
+
+    render() {
+      return (
+         <form id="add-book">
+             <div className="field"> 
+                <label>Book Name:</label>
+                <input type="text"></input>
+            </div>
+            <div className="field"> 
+                <label>Genre:</label>
+                <input type="text"></input>
+            </div>
+            <div className="field"> 
+                <label>Author:</label>
+                <select>
+                    <option>Select author</option>
+                    {this.displayAuthors()}
+                </select>  
+            </div>
+            <button>+</button>
+         </form>
+      );
+    }
+  }
+  
+  export default graphql(getAuthorsQuery)(AddBook);
+  
