6cf9a3201a0d92b10189e1deeb4120cdfb8253eb
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-06 14:19:55 +0530

    created book component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//components 
+import BookList from './components/BookList'
+
 class App extends Component {
   render() {
     return (
       <div id="main">
          <h1>Ninja's Reading Lists</h1>
+         <BookList />
       </div>
     );
   }
