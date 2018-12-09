e40186aac6c8d4c14c5b7d7a6a3023ece155fd43
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-09 15:21:12 +0530

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     return (
      <ApolloProvider client={client}>
         <div id="main">
-          <h1>Ninja's Reading List</h1>
+          <h1>Reading List</h1>
           <BookList />
           <AddBook />
         </div>
